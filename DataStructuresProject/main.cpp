2f6742015241acfb6c6997133c42f34391ec6a60
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2018-01-31 07:38:02 -0700

    Started my data structures project

---
+++ DataStructuresProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructuresProject
+//
+//  Created by Bevan, Tyler on 1/31/18.
+//  Copyright © 2018 Bevan, Tyler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
